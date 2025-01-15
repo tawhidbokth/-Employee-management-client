ad403509cf0f7a7fdc4bde1162a210d83c996948
shantodeveloper <ahmedshanto690@gmail.com>
2025-01-15 18:38:53 +0600

    navabr create

---
+++ src/Layout/MainLayout.jsx
@@ -0,0 +1,11 @@
+import Home from '../Components/Home/Home';
+
+const MainLayout = () => {
+  return (
+    <div>
+      <Home></Home>
+    </div>
+  );
+};
+
+export default MainLayout;
--- src/Router/router.jsx
+++ src/Router/router.jsx
@@ -1,9 +1,10 @@
 import { createBrowserRouter } from 'react-router-dom';
+import MainLayout from '../Layout/MainLayout';
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <div>Hello world!</div>,
+    element: <MainLayout></MainLayout>,
   },
 ]);
 
